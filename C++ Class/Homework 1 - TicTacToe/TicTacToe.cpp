d1d92b02c54139e726bed1a1fb1c4bd8b60e38e2
Kevin Ku <kkuo12887@gmail.com>
2015-05-20 04:31:30 -0400

    Initial Commit

---
+++ C++ Class/Homework 1 - TicTacToe/TicTacToe.cpp
@@ -0,0 +1,21 @@
+/*
+ * TicTacToe.cpp
+ *
+ *  Created on: May 20, 2015
+ *      Author: Kevin Kuo
+ */
+
+#include <array>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "Welcome to the Tic Tac Toe Game" << endl;
+	char player;
+
+}
+
+
---
+++ C++ Class/Homework 1 - TicTacToe/TicTacToe.h
@@ -0,0 +1,15 @@
+/*
+ * TicTacToe.h
+ *
+ *  Created on: May 20, 2015
+ *      Author: J14688
+ */
+
+#ifndef TICTACTOE_H_
+#define TICTACTOE_H_
+
+
+
+
+
+#endif /* TICTACTOE_H_ */

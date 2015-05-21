be466952b1249347ebe1c502f2810012759fd568
Kevin Ku <kkuo12887@gmail.com>
2015-05-21 18:07:18 -0400

    Updates and addition of homework assignment.

--- C++ Class/Homework 1 - TicTacToe/TicTacToe.h
+++ C++ Class/Homework 1 - TicTacToe/TicTacToe.h
@@ -8,7 +8,24 @@
 #ifndef TICTACTOE_H_
 #define TICTACTOE_H_
 
+using namespace std;
 
+class TicTacToe
+{
+public:
+
+	TicTacToe();
+	~TicTacToe();
+
+	void drawBoard(const int &row, const int &col) const;
+	bool isValidMove();
+	void recordMOve();
+	int isWinner();
+	int isDraw();
+
+private:
+
+};
 
 
 
